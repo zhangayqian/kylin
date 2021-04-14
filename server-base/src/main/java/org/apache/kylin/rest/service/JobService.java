81ffb037efb07c0c4f2ba48154375dab32720d26
yaqian.zhang <598593183@qq.com>
2021-04-14 20:05:07 +0800

    fix

--- server-base/src/main/java/org/apache/kylin/rest/service/JobService.java
+++ server-base/src/main/java/org/apache/kylin/rest/service/JobService.java
@@ -503,7 +503,7 @@ public class JobService extends BasicService implements InitializingBean {
 
     public String getJobStepOutput(String jobId, String stepId) {
         ExecutableManager executableManager = getExecutableManager();
-        AbstractExecutable job = executableManager.getJob(stepId);
+        AbstractExecutable job = executableManager.getJob(jobId);
         if (job instanceof CheckpointExecutable) {
             return executableManager.getOutput(stepId).getVerboseMsg();
         }
@@ -512,7 +512,7 @@ public class JobService extends BasicService implements InitializingBean {
 
     public String getAllJobStepOutput(String jobId, String stepId) {
         ExecutableManager executableManager = getExecutableManager();
-        AbstractExecutable job = executableManager.getJob(stepId);
+        AbstractExecutable job = executableManager.getJob(jobId);
         if (job instanceof CheckpointExecutable) {
             return executableManager.getOutput(stepId).getVerboseMsg();
         }
